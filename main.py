aad4de378a8870616ee61e1e7962addb2848700b
Jibran Syed <jibran.syed77@gmail.com>
2024-06-20 16:13:45 -0400

    get some of the user input stuff done

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import os
+import sys
+
+def restart_script(num_lines):
+    for _ in range(num_lines):
+        # Move the cursor up one line and clear it
+        sys.stdout.write("\033[F")  # Move cursor up one line
+        sys.stdout.write("\033[K")  # Clear the line
+    sys.stdout.flush()  # Ensure the commands are executed immediately
+    
+    # Full path to the Python executable
+    python_executable = sys.executable
+    
+    # Full path to the script
+    script_path = os.path.abspath(sys.argv[0])
+    
+    # Restart the script
+    os.execv(python_executable, [python_executable, script_path] + sys.argv[1:])
 
 def user_input():
     usrKeyboardName = input ("Enter the keyboard name: ")
@@ -14,22 +31,53 @@ def user_input():
     else:
         print ("Invalid input, Please answer with 'y' or 'n'")
 
-    return isSplit, usrKeyboardName
+    print("[0] nice!nano")
+    print("[1] XIAO BLE nrf52840")
+    usrMCUchoice = input("Select MCU: ")
 
-def file_creation(issPlit):
+    # Convert the user input to an integer
+    try:
+        usrMCUchoice = int(usrMCUchoice)
+        
+        if usrMCUchoice == 0:
+            usrMCUchoice = "nice!nano"
+            print(usrMCUchoice)
+            # sets nice!nano as the mcu
+        elif usrMCUchoice == 1:
+            usrMCUchoice = "XIAO BLE nrf52840"
+            print(usrMCUchoice)
+            # sets the xiaoble as the mcu
+        else:
+            print("Invalid selection")
+    except ValueError:
+        print("Please enter a valid number [between 0 & 1]")
+
+    return isSplit, usrKeyboardName, usrMCUchoice
+
+def file_creation(split, kbdnm, mcuSelec):
     #put file & folder heiarchy creation here
-    if issPlit == true:
+    if issPlit == True:
         #create files with regards to a split keyboard
-        print(issPlit)
-    elif issPlit == false:
+        print(split)
+        print(kbdnm)
+    elif issPlit == False:
         #create files with regards to a monoblock keyboard
-        print (issPlit)
+        print(split)
+        print(kbdnm)
     else:
-        print("something went wrong. Restart the script please")
+        print("something went wrong. please restart the script")
 
 def matrix_generation(rows,cols,transform):
     #put matrix generation stuff here
     pass
 
-issPlit, keyboardName = user_input() 
-print (f"Keyboard Name: {keyboardName},  \nSplit?: {issPlit}")
+issPlit, keyboardName, mcuChoice = user_input() 
+print (f"MCU: {mcuChoice} \nKeyboard Name: {keyboardName} \nSplit?: {issPlit}") 
+userConfirmation = input("Is this correct? (y/n) : ")
+if userConfirmation == 'y':
+    file_creation(issPlit,keyboardName)
+elif userConfirmation == 'n':
+    print("restarting script")
+    restart_script(15)
+else: 
+    print("something went wrong. restart the script.")
